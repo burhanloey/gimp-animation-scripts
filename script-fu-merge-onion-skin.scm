5f83d2d6ab65494608db3a4c6433111503eaea20
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-03-05 08:59:54 +0800

    Refactor

--- script-fu-merge-onion-skin.scm
+++ script-fu-merge-onion-skin.scm
@@ -1,82 +1,90 @@
 (define (script-fu-merge-onion-skin image)
-  (let ((parasites (cadr (gimp-image-get-parasite-list image))))
 
-    (if (not (member "onion-skinning-parent-id" parasites))
-        (gimp-message
-         "This image is not the image for onion skinning.")
+  (define +id+ "onion-skinning-id")
+  (define +parent-id+ "onion-skinning-parent-id")
 
-        (let* ((parent-id (caddr
-                           (car
-                            (gimp-image-get-parasite
-                             image
-                             "onion-skinning-parent-id"))))
-               (images (cadr (gimp-image-list)))
-               (parent-image (foldr
-                              (lambda (x y) (if (null? x) y x))
-                              nil
-                              (map
-                               (lambda (image)
-                                 (let ((parasites (cadr
-                                                   (gimp-image-get-parasite-list
-                                                    image))))
-                                   (if (member "onion-skinning-id" parasites)
-                                       (let ((id (caddr
-                                                  (car
-                                                   (gimp-image-get-parasite
-                                                    image
-                                                    "onion-skinning-id")))))
-                                         (if (equal? id parent-id)
-                                             image)))))
-                               (vector->list images)))))
+  (define (get-parasites image)
+    (cadr (gimp-image-get-parasite-list image)))
 
-          (if (null? parent-image)
-              (gimp-message
-               "The image to which this onion skins belong to is not opened.")
+  (define (get-parasite image name)
+    (caddr
+     (car
+      (gimp-image-get-parasite image name))))
 
-              (let* ((top-frame (vector-ref
-                                 (cadr (gimp-image-get-layers image)) 0))
-                     (frame-name (car (gimp-item-get-name top-frame)))
+  (define (get-layers image)
+    (cadr (gimp-image-get-layers image)))
 
-                     (parent-frames (cadr (gimp-image-get-layers parent-image)))
+  (define (find-if predicate alist)
+    (foldr
+     (lambda (x y) (if (null? x) y x))
+     nil
+     (map
+      (lambda (item) (if (predicate item) item))
+      alist)))
 
-                     (original-frame
-                      (foldr
-                       (lambda (x y) (if (null? x) y x))
-                       nil
-                       (map
-                        (lambda (frame)
-                          (if (equal? frame-name (car (gimp-item-get-name frame)))
-                              frame))
-                        (vector->list parent-frames))))
+  (define (vector-first avector)
+    (vector-ref avector 0))
 
-                     (original-position (car
-                                         (gimp-image-get-item-position
-                                          parent-image
-                                          original-frame))))
+  (define (vector-last avector)
+    (vector-ref avector (- (vector-length avector) 1)))
 
-                (let* ((new-frame (car
-                                   (gimp-layer-new-from-drawable
-                                    top-frame
-                                    parent-image)))
-                       (children (gimp-item-get-children new-frame))
-                       (num-new-layers (car children))
-                       (new-layers (cadr children))
-                       (background (vector-ref new-layers (- num-new-layers 1))))
+  (define (get-name item)
+    (car (gimp-item-get-name item)))
 
-                  (gimp-item-set-name
-                   new-frame
-                   (car
-                    (gimp-item-get-name top-frame)))
 
-                  (gimp-item-set-visible background TRUE)
 
-                  (gimp-image-remove-layer parent-image original-frame)
+  (if (not (member +parent-id+ (get-parasites image)))
+      (gimp-message
+       "This image is not the image for onion skinning.")
 
-                  (gimp-image-insert-layer
-                   parent-image
-                   new-frame
-                   0
-                   original-position))))))))
+      (let* ((parent-id (get-parasite image +parent-id+))
+             (parent-image (find-if
+                            (lambda (image)
+                              (and
+                               (member +id+ (get-parasites image))
+                               (equal? (get-parasite image +id+) parent-id)))
+                            (vector->list (cadr (gimp-image-list))))))
+
+        (if (null? parent-image)
+            (gimp-message
+             (string-append
+              "The image to which this onion skins belong to is not opened. "
+              "If you open it again and it still does not work, you probably "
+              "forgot to save and the reference metadata is gone."))
+
+            (let* ((top-frame (vector-first (get-layers image)))
+                   (top-frame-name (get-name top-frame))
+
+                   (original-frame (find-if
+                                    (lambda (frame)
+                                      (equal? (get-name frame) top-frame-name))
+                                    (vector->list (get-layers parent-image))))
+
+                   (original-position (car
+                                       (gimp-image-get-item-position
+                                        parent-image
+                                        original-frame)))
+
+                   (new-frame (car
+                               (gimp-layer-new-from-drawable
+                                top-frame
+                                parent-image)))
+
+                   (background (vector-last
+                                (cadr
+                                 (gimp-item-get-children new-frame)))))
+
+              (gimp-item-set-name new-frame top-frame-name)
+
+              (gimp-item-set-visible background TRUE)
+
+              (gimp-image-remove-layer parent-image original-frame)
+
+              (gimp-image-insert-layer
+               parent-image
+               new-frame
+               0
+               original-position))))))
 
 
 ;; Registering script
